700ff7317e6260907e5d17bf62eb8c231522a051
kleimj1 <jochen.kleimann@gmail.com>
2024-07-13 00:25:50 +0200

    Update const.py

--- custom_components/dbs2300/const.py
+++ custom_components/dbs2300/const.py
@@ -1,3 +1,14 @@
-"""Constants for the DBS2300 integration."""
-
 DOMAIN = "dbs2300"
+
+CONF_API_KEY = "local_key"
+
+SENSOR_TYPES = {
+    "ac_input_power": "AC Input Power",
+    "ac_output_power": "AC Output Power",
+    "dc_output_power": "DC Output Power",
+    "battery_capacity": "Battery Capacity",
+}
+
+SWITCH_TYPES = {
+    "ac_output_control": "AC Output Control"
+}
