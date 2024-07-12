c556ad990d63b33853b1f67bd503690dec93f68a
kleimj1 <jochen.kleimann@gmail.com>
2024-07-13 00:04:12 +0200

    Update sensor.py

--- custom_components/dbs2300/sensor.py
+++ custom_components/dbs2300/sensor.py
@@ -1,39 +1,36 @@
-"""Platform for sensor integration."""
+import logging
 from homeassistant.helpers.entity import Entity
-import tinytuya
 from .const import DOMAIN
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the sensor platform."""
-    host = hass.data[DOMAIN]['host']
-    device_id = hass.data[DOMAIN]['device_id']
-    local_key = hass.data[DOMAIN]['local_key']
-    
-    sensors = []
-    sensors.append(DBS2300Sensor(host, device_id, local_key))
-    async_add_entities(sensors, True)
+_LOGGER = logging.getLogger(__name__)
 
-class DBS2300Sensor(Entity):
-    """Representation of a Sensor."""
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Dabbsson DBS2300 sensors."""
+    config = hass.data[DOMAIN][config_entry.entry_id]
+    entities = [
+        DabbssonSensor(config, "AC Output Power", "ac_output_power"),
+        DabbssonSensor(config, "DC Output Power", "dc_output_power"),
+        DabbssonSensor(config, "Battery Capacity", "battery_capacity"),
+    ]
+    async_add_entities(entities, True)
 
-    def __init__(self, host, device_id, local_key):
-        """Initialize the sensor."""
+class DabbssonSensor(Entity):
+    """Representation of a Dabbsson DBS2300 sensor."""
+
+    def __init__(self, config, name, sensor_type):
+        self._config = config
+        self._name = name
+        self._type = sensor_type
         self._state = None
-        self._name = "DBS2300 Sensor"
-        self.device = tinytuya.OutletDevice(device_id, host, local_key)
-        self.device.set_version(3.3)
 
     @property
     def name(self):
-        """Return the name of the sensor."""
         return self._name
 
     @property
     def state(self):
-        """Return the state of the sensor."""
         return self._state
 
-    async def async_update(self):
-        """Fetch new state data for the sensor."""
-        status = self.device.status()
-        self._state = status['dps']['1']  # Beispiel: Status des AC Output Power
+    def update(self):
+        # Hier die tinytuya Logik zum Abrufen der Sensorwerte einfügen
+        pass
