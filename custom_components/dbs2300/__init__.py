43baadc1421cf27ed13eccfe77c9cd5420f51850
kleimj1 <jochen.kleimann@gmail.com>
2024-07-13 00:02:48 +0200

    Update __init__.py

--- custom_components/dbs2300/__init__.py
+++ custom_components/dbs2300/__init__.py
@@ -1,20 +1,31 @@
-"""DBS2300 component setup."""
 import logging
-from homeassistant.helpers import discovery
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup(hass, config):
-    """Set up the DBS2300 component."""
-    conf = config[DOMAIN]
-    hass.data[DOMAIN] = {
-        'host': conf['host'],
-        'device_id': conf['device_id'],
-        'local_key': conf['local_key']
-    }
-    
-    discovery.load_platform(hass, 'sensor', DOMAIN, {}, config)
-    discovery.load_platform(hass, 'switch', DOMAIN, {}, config)
-    
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Dabbsson DBS2300 component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Dabbsson DBS2300 from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "switch")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    await hass.config_entries.async_forward_entry_unload(entry, "switch")
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
