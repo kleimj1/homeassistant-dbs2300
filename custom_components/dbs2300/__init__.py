1f40283df602264247584cd672c3f48be5f4f1cb
kleimj1 <jochen.kleimann@gmail.com>
2024-07-13 00:49:23 +0200

    Update __init__.py

--- custom_components/dbs2300/__init__.py
+++ custom_components/dbs2300/__init__.py
@@ -1,35 +1,59 @@
-from homeassistant.helpers import discovery
-from .const import DOMAIN
+import asyncio
+import logging
+import async_timeout
 
-async def async_setup(hass, config):
-    """Set up the DBS2300 component."""
-    hass.data[DOMAIN] = {}
-    return True
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import DOMAIN, SCAN_INTERVAL
+
+PLATFORMS = ["sensor", "switch"]
 
 async def async_setup_entry(hass, entry):
     """Set up DBS2300 from a config entry."""
-    data = entry.data
-    host = data['host']
-    device_id = data['device_id']
-    local_key = data.get('local_key')
+    hass.data.setdefault(DOMAIN, {})
 
-    if data.get('use_secrets'):
-        local_key = hass.secrets.get(local_key, local_key)
+    coordinator = DBS2300DataUpdateCoordinator(
+        hass, entry.data
+    )
 
-    hass.data[DOMAIN][entry.entry_id] = {
-        'host': host,
-        'device_id': device_id,
-        'local_key': local_key
-    }
+    await coordinator.async_refresh()
 
-    for component in ("sensor", "switch"):
-        hass.async_create_task(
-            discovery.async_load_platform(hass, component, DOMAIN, {}, entry.data)
+    if not coordinator.last_update_success:
+        raise ConfigEntryNotReady
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    for platform in PLATFORMS:
+        hass.async_add_job(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
         )
 
+    entry.add_update_listener(async_reload_entry)
     return True
 
-async def async_unload_entry(hass, entry):
-    """Unload a config entry."""
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+class DBS2300DataUpdateCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching DBS2300 data."""
+
+    def __init__(self, hass, config):
+        """Initialize."""
+        self.config = config
+        self.api = DBS2300API(config)
+
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=DOMAIN,
+            update_interval=timedelta(seconds=10),
+        )
+
+    async def _async_update_data(self):
+        """Update data via library."""
+        try:
+            async with async_timeout.timeout(10):
+                return await self.api.get_data()
+        except Exception as error:
+            raise UpdateFailed(f"Error communicating with API: {error}")
+
+async def async_reload_entry(hass, entry):
+    """Reload config entry."""
+    await hass.config_entries.async_reload(entry.entry_id)
