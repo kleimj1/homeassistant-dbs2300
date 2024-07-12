5e9f8eae74654b02cfbf939351d1aa8f666c7893
kleimj1 <jochen.kleimann@gmail.com>
2024-07-13 00:58:12 +0200

    Update const.py

--- custom_components/dbs2300/const.py
+++ custom_components/dbs2300/const.py
@@ -1,16 +1,14 @@
-from datetime import timedelta
-
 DOMAIN = "dbs2300"
-SCAN_INTERVAL = timedelta(seconds=10)
-
 
 SENSOR_TYPES = {
+    "ac_input_voltage": "AC Input Voltage",
     "ac_input_power": "AC Input Power",
-    "ac_output_power": "AC Output Power",
-    "dc_output_power": "DC Output Power",
+    "dc_input_voltage": "DC Input Voltage",
+    "dc_input_power": "DC Input Power",
     "battery_capacity": "Battery Capacity",
 }
 
 SWITCH_TYPES = {
-    "ac_output_control": "AC Output Control"
+    "ac_output": "AC Output",
+    "dc_output": "DC Output",
 }
