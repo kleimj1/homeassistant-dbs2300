821f255145d51cd4cc1a1ba39fefbdb0253b6d84
kleimj1 <jochen.kleimann@gmail.com>
2024-07-13 00:25:27 +0200

    Update __init__.py

--- custom_components/dbs2300/__init__.py
+++ custom_components/dbs2300/__init__.py
@@ -1,5 +1,10 @@
+import logging
 from homeassistant.helpers import discovery
+from homeassistant.const import CONF_HOST, CONF_DEVICE_ID, CONF_API_KEY
+
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
     """Set up the DBS2300 component."""
@@ -8,7 +13,20 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass, entry):
     """Set up DBS2300 from a config entry."""
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    data = entry.data
+
+    host = data[CONF_HOST]
+    device_id = data[CONF_DEVICE_ID]
+    local_key = data.get(CONF_API_KEY)
+
+    if data.get("use_secrets"):
+        local_key = hass.secrets.get(local_key, local_key)
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        CONF_HOST: host,
+        CONF_DEVICE_ID: device_id,
+        CONF_API_KEY: local_key
+    }
 
     for component in ("sensor", "switch"):
         hass.async_create_task(
